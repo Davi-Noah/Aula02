bd7c65fdf4a09b7e1afcae15fe3ddf00b6ba3dba
Davi-Noah <daviollee@gmail.com>
2024-12-14 17:40:25 -0300

    Privando dados

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 class ContaBancaria:
-    def __init__(self, titular, saldo):
-        self.titular = titular
-        self.saldo = saldo
+    def __init__(self, titular, saldo = 0):
+        self._titular = titular
+        self.__saldo = saldo
     
     def depositar(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def sacar(self, valor):
-        if(self.saldo >= valor):
-            self.saldo -= valor
+        if(self.__saldo >= valor):
+            self.__saldo -= valor
         else:
             print('Impossivel realizar a operação')
 
     def verficar_saldo(self):
-        return f'O saldo atual é de {self.saldo}'
+        return f'O saldo atual é de {self.__saldo}'
+    
+    def set_saldo(self, valor):
+        self.__saldo = valor
+    
+
+minhaConta = ContaBancaria('Davi')
+minhaConta.depositar(1000)
+print(minhaConta.verficar_saldo())
     
